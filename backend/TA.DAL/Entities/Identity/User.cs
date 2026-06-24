bc325cac8f157ace3b5746807bfe7cf9a9d0dbca
Am0rr <itsaizel468@gmail.com>
2026-06-24 17:24:26 +0300

    feat(DAL): add User entitiy and UseRole enum

---
+++ backend/TA.DAL/Entities/Identity/User.cs
@@ -0,0 +1,25 @@
+using TA.DAL.Enums;
+
+namespace TA.DAL.Entities;
+
+public class User : BaseEntity
+{
+    public string Username { get; private set; } = null!;
+    public string Email { get; private set; } = null!;
+    public string PasswordHash { get; private set; } = null!;
+    public UserRole Role { get; private set; }
+
+    protected User() { }
+
+    public User(string username, string email, string passwordHash, UserRole role = UserRole.User)
+    {
+        Username = username;
+        Email = email;
+        PasswordHash = passwordHash;
+        Role = role;
+    }
+
+    public void ChangeUsername(string newUsername) => Username = newUsername;
+    public void ChangeEmail(string newEmail) => Email = newEmail;
+    public void ChangeRole(UserRole newRole) => Role = newRole;
+}
