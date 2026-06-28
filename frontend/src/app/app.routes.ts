2e27804f50eb7eb3339a9b2076ee8a88f48cd96f
Am0rr <itsaizel468@gmail.com>
2026-06-28 19:06:31 +0300

    feat(frontend): implement static tasks component and configure it in app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)}
+    {path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)},
+    {path: 'tasks', loadComponent: () => import('./features/tasks/tasks.component').then(m => m.TasksComponent)},
 ];
