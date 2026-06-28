b976aa57e39d04f9345fa604eee35e77ddf13487
Am0rr <itsaizel468@gmail.com>
2026-06-28 18:47:39 +0300

    feat(frontend): add logic to login.component

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "../../../core/services/auth.service";
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [],
+    imports: [FormsModule, RouterLink],
     templateUrl: './login.component.html',
 })
 
 export class LoginComponent {
-    
+    email = '';
+    password = '';
+    error = '';
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    onSubmit() {
+        this.authService.login({email: this.email, password: this.password}).subscribe({
+            next: (response) => {
+                this.authService.saveTokens(response);
+                this.router.navigate(['/tasks']);
+            },
+            error: () => {
+                this.error = 'Invalid email or password';
+            }
+        }); 
+    }
 }
