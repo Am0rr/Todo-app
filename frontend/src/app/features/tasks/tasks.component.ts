6409af9ebe6cba4229f28c1de90757d6a3556863
Am0rr <itsaizel468@gmail.com>
2026-06-29 13:30:14 +0300

    feat(frontend): implement searching logic in tasks.component

--- frontend/src/app/features/tasks/tasks.component.ts
+++ frontend/src/app/features/tasks/tasks.component.ts
@@ -1,43 +1,61 @@
-import { Component, OnInit } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { TaskFilterModel, TaskPagedResponse, TaskResponse } from "../../core/models/task.model";
-import { CategoryResponse } from "../../core/models/category.model";
-import { TaskService } from "../../core/services/task.service";
-import { CategoryService } from "../../core/services/category.service";
-import { AsyncPipe } from "@angular/common";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskFilterModel, TaskItemStatus, TaskPagedResponse } from '../../core/models/task.model';
+import { CategoryResponse } from '../../core/models/category.model';
+import { TaskService } from '../../core/services/task.service';
+import { CategoryService } from '../../core/services/category.service';
+import { AsyncPipe } from '@angular/common';
+import { BehaviorSubject, debounceTime, Observable, shareReplay, switchMap } from 'rxjs';
 
 @Component({
-    selector: 'app-tasks',
-    standalone: true,
-    imports: [FormsModule, AsyncPipe],
-    templateUrl: './tasks.component.html'
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [FormsModule, AsyncPipe],
+  templateUrl: './tasks.component.html',
 })
-
 export class TasksComponent implements OnInit {
-    tasks$!: Observable<TaskPagedResponse>;
-    categories$!: Observable<CategoryResponse[]>;
+  tasks$!: Observable<TaskPagedResponse>;
+  categories$!: Observable<CategoryResponse[]>;
 
-    filter: TaskFilterModel = {
-        pageNumber: 1,
-        pageSize: 10,
-    }
+  constructor(
+    private taskService: TaskService,
+    private categoryService: CategoryService,
+  ) {}
 
-    constructor(
-        private taskService: TaskService,
-        private categoryService: CategoryService,
-    ) {}
+  private filterSubject = new BehaviorSubject<TaskFilterModel>({
+    pageNumber: 1,
+    pageSize: 10,
+  });
 
-    ngOnInit() {
-        this.loadTasks();
-        this.loadCategories();
-    }
+  ngOnInit() {
+    this.categories$ = this.categoryService.getAll().pipe(shareReplay(1));
 
-    loadTasks() {
-        this.tasks$ = this.taskService.getPaged(this.filter);
-    }
+    this.tasks$ = this.filterSubject.pipe(
+      debounceTime(300),
+      switchMap((filter) => this.taskService.getPaged(filter)),
+      shareReplay(1),
+    );
+  }
 
-    loadCategories() {
-        this.categories$ = this.categoryService.getAll();
-    }
+  onSearch(event: Event) {
+    const searchTerm = (event.target as HTMLInputElement).value || undefined;
+    this.filterSubject.next({
+      ...this.filterSubject.value,
+      searchTerm,
+      pageNumber: 1,
+    });
+  }
+
+  onStatusChange(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.filterSubject.next({
+      ...this.filterSubject.value,
+      status: value ? (value as TaskItemStatus) : undefined,
+      pageNumber: 1,
+    });
+  }
+
+  loadTasks() {
+    this.filterSubject.next(this.filterSubject.value);
+  }
 }
